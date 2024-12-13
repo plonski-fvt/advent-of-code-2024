fb8b743e0677538ba0a553bc742fe69a2295b0dd
Patrick A Plonski <patrick.plonski@metergroup.com>
2024-12-13 11:21:35 -0600

    implement more efficient method for day11b

--- day11/day11b.py
+++ day11/day11b.py
@@ -1,39 +1,38 @@
-# (x, b) describes the output of propagating a stone x, n times
-stone_dict: dict[tuple[int, int], list] = {}
-
-def propagate_n_times(stones: list[int], n: int) -> list[int]:
-    next_stones = []
-    if n == 1:
-        for stone in stones:
-            stone_str = str(stone)
-            if stone == 0:
-                next_stones.append(1)
-            elif (len(stone_str) % 2) == 0:
-                next_stones.append(int(stone_str[:int(len(stone_str)/2)]))
-                next_stones.append(int(stone_str[int(len(stone_str)/2):]))
-            else:
-                next_stones.append(stone * 2024)
-        return next_stones
-    for stone in stones:
-        resultant_stones = stone_dict.get((stone, n), None)
-        if resultant_stones is None:
-            # n1 = max(1, round(2 * n / 3))
-            n1 = 1
-            n2 = n - n1
-            resultant_stones = propagate_n_times(propagate_n_times([stone], n1), n2)
-            stone_dict[(stone, n)] = resultant_stones
-        next_stones = next_stones + resultant_stones
+def propagate_dict(stones: dict[int, int]) -> dict[int, int]:
+    next_stones: dict[int, int] = {}
+    for stone, count in stones.items():
+        stone_str = str(stone)
+        if (stone == 0):
+            next_stone_count = next_stones.get(1, 0)
+            next_stones[1] = next_stone_count + count
+        elif (len(stone_str) % 2) == 0:
+            next_stone_a = int(stone_str[:int(len(stone_str)/2)])
+            next_stone_b = int(stone_str[int(len(stone_str)/2):])
+            next_count_a = next_stones.get(next_stone_a, 0)
+            next_stones[next_stone_a] = next_count_a + count
+            next_count_b = next_stones.get(next_stone_b, 0)
+            next_stones[next_stone_b] = next_count_b + count
+        else:
+            next_stone = stone * 2024
+            next_stone_count = next_stones.get(next_stone, 0)
+            next_stones[next_stone] = next_stone_count + count
     return next_stones
 
 file = open("day11-input.txt")
 
 stones = [int(x) for x in file.readline().strip().split()]
 
+stones_dict = {}
+for stone in stones:
+    current_count = stones_dict.get(stone, 0)
+    stones_dict[stone] = current_count + 1
+
 print(stones)
 print(len(stones))
+print(stones_dict)
 
 for i in range(0, 75, 1):
     print(i + 1)
-    resultant_stones = propagate_n_times(stones, i + 1)
-    print(len(resultant_stones))
-    print(len(stone_dict))
+    stones_dict = propagate_dict(stones_dict)
+    print(len(stones_dict))
+    print(sum(stones_dict.values()))
