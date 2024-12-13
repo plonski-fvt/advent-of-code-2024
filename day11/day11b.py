d22139f231d6ae38dfb056b0e493071d3eb2393e
Patrick A Plonski <patrick.plonski@metergroup.com>
2024-12-13 10:44:55 -0600

    Revert "use numpy arrays to improve memory efficiency"
    This reverts commit e3f018466ee77e089d4979b3842724501c704784.

--- day11/day11b.py
+++ day11/day11b.py
@@ -1,36 +1,30 @@
-import numpy as np
-
 # (x, b) describes the output of propagating a stone x, n times
-stone_dict: dict[tuple[int, int], np.ndarray] = {}
+stone_dict: dict[tuple[int, int], list] = {}
 
-def propagate_n_times(stones: np.ndarray, n: int) -> np.ndarray:
+def propagate_n_times(stones: list[int], n: int) -> list[int]:
     if n == 1:
-        next_stones = np.array([])
+        next_stones = []
         for stone in stones:
-            stone_str = str(int(stone))
+            stone_str = str(stone)
             if stone == 0:
-                next_stones = np.append(next_stones, 1)
+                next_stones.append(1)
             elif (len(stone_str) % 2) == 0:
-                next_stones = np.append(next_stones, 
-                                        np.array([int(stone_str[:int(len(stone_str)/2)]),
-                                        int(stone_str[int(len(stone_str)/2):])]))
+                next_stones.append(int(stone_str[:int(len(stone_str)/2)]))
+                next_stones.append(int(stone_str[int(len(stone_str)/2):]))
             else:
-                next_stones = np.append(next_stones, stone * 2024)
-        return np.array(next_stones)
-    next_stones_sublists: list[np.ndarray] = []
+                next_stones.append(stone * 2024)
+        return next_stones
+    next_stones_sublists = []
     for stone in stones:
         resultant_stones = stone_dict.get((stone, n), None)
         if resultant_stones is None:
-            # print("cache miss: {} {}".format(stone, n))
             # n1 = max(1, round(2 * n / 3))
             n1 = 1
             n2 = n - n1
-            resultant_stones = propagate_n_times(propagate_n_times(np.array([stone]), n1), n2)
-        # else:
-            # print("cache hit: {} {}".format(stone, n))
+            resultant_stones = propagate_n_times(propagate_n_times([stone], n1), n2)
         next_stones_sublists.append(resultant_stones)
     # print(next_stones_sublists)
-    next_stones = np.array([x for xs in next_stones_sublists for x in xs])
+    next_stones = [x for xs in next_stones_sublists for x in xs]
     # print(next_stones)
     index = 0
     # this should improve memory efficiency
@@ -48,6 +42,6 @@ print(len(stones))
 
 for i in range(75):
     print(i + 1)
-    resultant_stones = propagate_n_times(np.array(stones), i + 1)
+    resultant_stones = propagate_n_times(stones, i + 1)
     print(len(resultant_stones))
     # print(len(stone_dict))
