bd369fd8b7e0ce3bff57563851110e6eb61e34a8
Patrick A Plonski <patrick.plonski@metergroup.com>
2024-12-12 21:46:15 -0600

    don't use dictionary for n=1 since this slows things down in practice

--- day11/day11b.py
+++ day11/day11b.py
@@ -5,23 +5,22 @@ def propagate_n_times(stones: list[int], n: int) -> list[int]:
     next_stones = []
     if n == 1:
         for stone in stones:
-            resultant_stones = stone_dict.get((stone, n), None)
-            if resultant_stones is None:
-                stone_str = str(stone)
-                if stone == 0:
-                    resultant_stones = [1]
-                elif (len(stone_str) % 2) == 0:
-                    resultant_stones = [int(stone_str[:int(len(stone_str)/2)]),
-                                        int(stone_str[int(len(stone_str)/2):])]
-                else:
-                    resultant_stones = [stone * 2024]
-                stone_dict[(stone, n)] = resultant_stones
-            next_stones = next_stones + resultant_stones
+            stone_str = str(stone)
+            if stone == 0:
+                next_stones.append(1)
+            elif (len(stone_str) % 2) == 0:
+                next_stones.append(int(stone_str[:int(len(stone_str)/2)]))
+                next_stones.append(int(stone_str[int(len(stone_str)/2):]))
+            else:
+                next_stones.append(stone * 2024)
         return next_stones
     for stone in stones:
         resultant_stones = stone_dict.get((stone, n), None)
         if resultant_stones is None:
-            resultant_stones = propagate_n_times(propagate_n_times([stone], 1), n - 1)
+            # n1 = max(1, round(2 * n / 3))
+            n1 = 1
+            n2 = n - n1
+            resultant_stones = propagate_n_times(propagate_n_times([stone], n1), n2)
             stone_dict[(stone, n)] = resultant_stones
         next_stones = next_stones + resultant_stones
     return next_stones
@@ -33,7 +32,7 @@ stones = [int(x) for x in file.readline().strip().split()]
 print(stones)
 print(len(stones))
 
-for i in range(75):
+for i in range(0, 75, 1):
     print(i + 1)
     resultant_stones = propagate_n_times(stones, i + 1)
     print(len(resultant_stones))
